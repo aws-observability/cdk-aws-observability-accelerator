e9a9996323386e797980be745b45020573800fbd
Nagaraju <118366916+nrajb@users.noreply.github.com>
2023-07-22 12:09:22 +0000

    Updated addons based on testing

--- lib/existing-eks-awsnative-observability-construct/index.ts
+++ lib/existing-eks-awsnative-observability-construct/index.ts
@@ -34,14 +34,11 @@ export default class ExistingEksAwsNativeObservabilityConstruct {
         });
 
         const addOns: Array<blueprints.ClusterAddOn> = [
-            //new blueprints.addons.KubeProxyAddOn(),
-            //new blueprints.addons.AwsLoadBalancerControllerAddOn(),
-            //new blueprints.addons.CertManagerAddOn(),
             new blueprints.addons.CloudWatchLogsAddon({
                 logGroupPrefix: `/aws/eks/${stackId}`,
                 logRetentionDays: 30
             }),
-            // new blueprints.addons.ContainerInsightsAddOn(),
+            new blueprints.addons.ContainerInsightsAddOn(),
             //new blueprints.addons.XrayAddOn(),
         ];
 
