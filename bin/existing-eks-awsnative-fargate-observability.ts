cdce95a748b47a80cb3bcbb52e7a0ea84e9f77ab
Howlla <bhavye.sharma1996@gmail.com>
2023-08-22 11:35:42 -0700

    remove existing aws fargate code

--- bin/existing-eks-awsnative-fargate-observability.ts
+++
@@ -1,8 +0,0 @@
-import ExistingEksAwsNativeFargateObservabilityConstruct from '../lib/existing-eks-awsnative-fargate-observability-construct';
-import { configureApp, errorHandler } from '../lib/common/construct-utils';
-
-const app = configureApp();
-
-new ExistingEksAwsNativeFargateObservabilityConstruct().buildAsync(app, 'existing-eks-awsnative-fargate').catch((error) => {
-    errorHandler(app, "Existing Cluster Pattern is missing information of existing cluster: " + error);
-});
