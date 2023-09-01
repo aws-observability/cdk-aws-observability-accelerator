96dcb581bc2ce4c2d34d9eea7a865d6228c815d3
iamprakkie <gprakk.ie@gmail.com>
2023-08-31 19:20:34 -0600

    prod2

--- lib/multi-acc-new-eks-mixed-observability-pattern/cloudwatch-monitoring.ts
+++ lib/multi-acc-new-eks-mixed-observability-pattern/cloudwatch-monitoring.ts
@@ -34,17 +34,17 @@ export default class CloudWatchMonitoringConstruct {
         const account = contextAccount! || process.env.COA_ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
         const region = contextRegion! || process.env.COA_AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
 
-        const cloudWatchAdotAddOn = new blueprints.addons.CloudWatchAdotAddOn({
-            deploymentMode: cloudWatchDeploymentMode.DEPLOYMENT,
-            namespace: 'default',
-            name: 'adot-collector-cloudwatch',
-            metricsNameSelectors: ['apiserver_request_.*', 'container_memory_.*', 'container_threads', 'otelcol_process_.*', 'ho11y*'],
-            // podLabelRegex: 'frontend|downstream(.*)',
-        });        
+        // const cloudWatchAdotAddOn = new blueprints.addons.CloudWatchAdotAddOn({
+        //     deploymentMode: cloudWatchDeploymentMode.DEPLOYMENT,
+        //     namespace: 'default',
+        //     name: 'adot-collector-cloudwatch',
+        //     metricsNameSelectors: ['apiserver_request_.*', 'container_memory_.*', 'container_threads', 'otelcol_process_.*', 'ho11y*'],
+        //     // podLabelRegex: 'frontend|downstream(.*)',
+        // });        
 
         Reflect.defineMetadata("ordered", true, blueprints.addons.CloudWatchLogsAddon);        
         const addOns: Array<blueprints.ClusterAddOn> = [
-            cloudWatchAdotAddOn,            
+            // cloudWatchAdotAddOn,            
             new blueprints.addons.ClusterAutoScalerAddOn(),
             new blueprints.addons.SecretsStoreAddOn(),
             new blueprints.addons.CloudWatchLogsAddon({ 
