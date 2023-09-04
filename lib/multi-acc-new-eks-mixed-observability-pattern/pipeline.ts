6e2c0518266ac9616dc0808b476dc9cccb4b989a
iamprakkie <gprakk.ie@gmail.com>
2023-09-04 09:26:02 -0600

    AMG only

--- lib/multi-acc-new-eks-mixed-observability-pattern/pipeline.ts
+++ lib/multi-acc-new-eks-mixed-observability-pattern/pipeline.ts
@@ -100,33 +100,33 @@ export class PipelineMultiEnvMonitoring {
             .wave({
                 id: "prod-test",
                 stages: [
-                    {
-                        id: PROD1_ENV_ID,
-                        stackBuilder: blueprintAmp
-                            .name(PROD1_ENV_ID)                        
-                            .clone(context.prodEnv1.region, context.prodEnv1.account)
-                            .version('auto')
-                            .addOns(new blueprints.NestedStackAddOn({
-                                builder: AmpIamSetupStack.builder("ampPrometheusDataSourceRole", context.monitoringEnv.account!),
-                                id: "amp-iam-nested-stack"
-                            }))
-                            .addOns(
-                                prodArgoAddonConfig,
-                            )
-                    },
-                    {
-                        id: PROD2_ENV_ID,
-                        stackBuilder: blueprintCloudWatch
-                            .name(PROD2_ENV_ID)
-                            .clone(context.prodEnv2.region, context.prodEnv2.account)
-                            .addOns(new blueprints.NestedStackAddOn({
-                                builder: CloudWatchIamSetupStack.builder("cloudwatchDataSourceRole", context.monitoringEnv.account!),
-                                id: "cloudwatch-iam-nested-stack"
-                            }))
-                            .addOns(
-                                prodArgoAddonConfig,
-                            )
-                    },
+                    // {
+                    //     id: PROD1_ENV_ID,
+                    //     stackBuilder: blueprintAmp
+                    //         .name(PROD1_ENV_ID)                        
+                    //         .clone(context.prodEnv1.region, context.prodEnv1.account)
+                    //         .version('auto')
+                    //         .addOns(new blueprints.NestedStackAddOn({
+                    //             builder: AmpIamSetupStack.builder("ampPrometheusDataSourceRole", context.monitoringEnv.account!),
+                    //             id: "amp-iam-nested-stack"
+                    //         }))
+                    //         .addOns(
+                    //             prodArgoAddonConfig,
+                    //         )
+                    // },
+                    // {
+                    //     id: PROD2_ENV_ID,
+                    //     stackBuilder: blueprintCloudWatch
+                    //         .name(PROD2_ENV_ID)
+                    //         .clone(context.prodEnv2.region, context.prodEnv2.account)
+                    //         .addOns(new blueprints.NestedStackAddOn({
+                    //             builder: CloudWatchIamSetupStack.builder("cloudwatchDataSourceRole", context.monitoringEnv.account!),
+                    //             id: "cloudwatch-iam-nested-stack"
+                    //         }))
+                    //         .addOns(
+                    //             prodArgoAddonConfig,
+                    //         )
+                    // },
                     {
                         id: MON_ENV_ID,
                         stackBuilder: blueprintAmg
