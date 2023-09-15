525840159c5807034f791b5ccf96adaad9e4a9f3
howlla <bhavye.sharma1996@gmail.com>
2023-09-14 21:37:23 -0700

    Tested with updated observability builder props PR

--- lib/existing-eks-opensource-observability-pattern/index.ts
+++ lib/existing-eks-opensource-observability-pattern/index.ts
@@ -85,7 +85,8 @@ export default class ExistingEksOpenSourceobservabilityPattern {
             .account(account)
             .region(region)
             .version('auto')
-            .enableOpenSourcePatternAddOns(ampAddOnProps)
+            .withAmpProps(ampAddOnProps)
+            .enableOpenSourcePatternAddOns()
             .clusterProvider(importClusterProvider)
             .resourceProvider(blueprints.GlobalResources.Vpc, new blueprints.VpcProvider(vpcId)) // this is required with import cluster provider
             .resourceProvider(ampWorkspaceName, new blueprints.CreateAmpProvider(ampWorkspaceName, ampWorkspaceName))
