fae7a72f89b8ec19dec40f7fc2b053047aaf78b0
Riccardo Freschi <freschri@amazon.com>
2023-09-25 10:21:01 +0200

    added docs, cleanup

--- bin/single-new-eks-gpu-observability.ts
+++
@@ -1,6 +0,0 @@
-import SingleNewEksGpuObservabilityPattern from '../lib/single-new-eks-gpu-observability-pattern';
-import { configureApp } from '../lib/common/construct-utils';
-
-const app = configureApp();
-
-new SingleNewEksGpuObservabilityPattern(app, 'single-new-eks-gpu');
--- lib/single-new-eks-opensource-observability-pattern/gpu-index.ts
+++ lib/single-new-eks-opensource-observability-pattern/gpu-index.ts
@@ -1,37 +1,3 @@
-// import { GpuBuilder, GpuOptions } from "@aws-quickstart/eks-blueprints";
-// import * as ec2 from "aws-cdk-lib/aws-ec2";
-// import * as eks from "aws-cdk-lib/aws-eks";
-// import { Construct } from "constructs";
-
-// export default class SingleNewEksGpuObservabilityPattern {
-//     constructor(scope: Construct, id: string) {
-//         const account = process.env.CDK_DEFAULT_ACCOUNT!;
-//         const region = process.env.CDK_DEFAULT_REGION!;
-//         const stackID = `${id}-eks-blueprint`;
-
-//         const options: GpuOptions = {
-//             kubernetesVersion: eks.KubernetesVersion.V1_27,
-//             instanceClass: ec2.InstanceClass.G4DN,
-//             instanceSize: ec2.InstanceSize.XLARGE,
-//             desiredNodeSize: 1,
-//             minNodeSize: 1,
-//             maxNodeSize: 3,
-//         };
-
-//         const values = {
-//             driver: {
-//                 enabled: false
-//             }
-//         };
-
-//         GpuBuilder.builder(options)
-//             .account(account)
-//             .region(region)
-//             .enableGpu(values)
-//             .build(scope, stackID);
-//     }
-// }
-
 import { Construct } from 'constructs';
 import { utils } from '@aws-quickstart/eks-blueprints';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
@@ -40,9 +6,16 @@ import * as amp from 'aws-cdk-lib/aws-aps';
 import { ObservabilityBuilder } from '@aws-quickstart/eks-blueprints';
 import * as eks from "aws-cdk-lib/aws-eks";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as fs from 'fs';
 
-export default class SingleNewEksGpuObservabilityPattern {
+interface GpuNodeGroupProps {
+    instanceType: string,
+    desiredSize: number, 
+    minSize: number, 
+    maxSize:number,
+    ebsSize: number
+}
+
+export default class SingleNewEksGpuOpenSourceObservabilityPattern {
     constructor(scope: Construct, id: string) {
         
         const stackId = `${id}-observability-accelerator`;
@@ -73,50 +46,9 @@ export default class SingleNewEksGpuObservabilityPattern {
             }
         };
 
-        const jsonString = fs.readFileSync(__dirname + '/../../cdk.json', 'utf-8');
-        const jsonStringnew = JSON.parse(jsonString);
-        let doc = utils.readYamlDocument(__dirname + '/../common/resources/otel-collector-config.yml');
-        doc = utils.changeTextBetweenTokens(
-            doc,
-            "{{ if enableAPIserverJob }}",
-            "{{ end }}",
-            jsonStringnew.context["apiserver.pattern.enabled"]
-        );
-        console.log(doc);
-        fs.writeFileSync(__dirname + '/../common/resources/otel-collector-config.yml', doc);
-
-        if (utils.valueFromContext(scope, "java.pattern.enabled", false)) {
-            ampAddOnProps.openTelemetryCollector = {
-                manifestPath: __dirname + '/../common/resources/otel-collector-config.yml',
-                manifestParameterMap: {
-                    javaScrapeSampleLimit: 1000,
-                    javaPrometheusMetricsEndpoint: "/metrics"
-                }
-            };
-            ampAddOnProps.ampRules?.ruleFilePaths.push(
-                __dirname + '/../common/resources/amp-config/java/alerting-rules.yml',
-                __dirname + '/../common/resources/amp-config/java/recording-rules.yml'
-            );
-        }
-
-        if (utils.valueFromContext(scope, "apiserver.pattern.enabled", false)) {
-            ampAddOnProps.enableAPIServerJob = true,
-            ampAddOnProps.ampRules?.ruleFilePaths.push(
-                __dirname + '/../common/resources/amp-config/apiserver/recording-rules.yml'
-            );
-        }
-
-        if (utils.valueFromContext(scope, "nginx.pattern.enabled", false)) {
-            ampAddOnProps.openTelemetryCollector = {
-                manifestPath: __dirname + '/../common/resources/otel-collector-config.yml',
-                manifestParameterMap: {
-                    javaScrapeSampleLimit: 1000,
-                    javaPrometheusMetricsEndpoint: "/metrics"
-                }
-            };
-            ampAddOnProps.ampRules?.ruleFilePaths.push(
-                __dirname + '/../common/resources/amp-config/nginx/alerting-rules.yml'
-            );
+        const gpuNodeGroup: GpuNodeGroupProps = utils.valueFromContext(scope, "gpuNodeGroup", undefined);
+        if (gpuNodeGroup === undefined) {
+            throw new Error("Missing node group configuration");
         }
 
         Reflect.defineMetadata("ordered", true, blueprints.addons.GrafanaOperatorAddon);
@@ -132,7 +64,7 @@ export default class SingleNewEksGpuObservabilityPattern {
             new blueprints.addons.GpuOperatorAddon({
                 values: {
                     driver: {
-                        enabled: false
+                        enabled: false // the GPU driver is already embedded into the AL2_X86_64_GPU AMI image 
                     }
                 }
             })
@@ -152,7 +84,7 @@ export default class SingleNewEksGpuObservabilityPattern {
                         "Type": "generic-gpu-cluster"
                     },
                     managedNodeGroups: [
-                        addGpuNodeGroup(),
+                        addGpuNodeGroup(gpuNodeGroup),
                     ]
                 })
             )
@@ -161,15 +93,14 @@ export default class SingleNewEksGpuObservabilityPattern {
     }
 }
 
-function addGpuNodeGroup(): blueprints.ManagedNodeGroup {
-
+function addGpuNodeGroup(gpuNodeGroupProps: GpuNodeGroupProps): blueprints.ManagedNodeGroup {
     return {
         id: "mng-linux-gpu-01",
         amiType: eks.NodegroupAmiType.AL2_X86_64_GPU,
-        instanceTypes: [new ec2.InstanceType(`${ec2.InstanceClass.G4DN}.${ec2.InstanceSize.XLARGE}`)],
-        desiredSize: 2, 
-        minSize: 2, 
-        maxSize:3,
+        instanceTypes: [new ec2.InstanceType(gpuNodeGroupProps.instanceType)],
+        desiredSize: gpuNodeGroupProps.desiredSize, 
+        minSize: gpuNodeGroupProps.minSize, 
+        maxSize: gpuNodeGroupProps.maxSize,
         nodeGroupSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
         launchTemplate: {
             tags: {
@@ -181,7 +112,7 @@ function addGpuNodeGroup(): blueprints.ManagedNodeGroup {
             blockDevices: [
                 {
                     deviceName: "/dev/sda1",
-                    volume: ec2.BlockDeviceVolume.ebs(50),
+                    volume: ec2.BlockDeviceVolume.ebs(gpuNodeGroupProps.ebsSize),
                 }
             ]
         }
