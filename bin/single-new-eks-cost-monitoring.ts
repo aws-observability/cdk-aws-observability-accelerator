67e08a4c07b7dce1f7919825213148b217c2bda0
Arun <atthnga@amazon.com>
2024-03-11 23:28:37 -0500

    adds cognito auth to kubecost dashboard

--- bin/single-new-eks-cost-monitoring.ts
+++ bin/single-new-eks-cost-monitoring.ts
@@ -1,6 +1,11 @@
-import { configureApp } from '../lib/common/construct-utils';
+import { configureApp, errorHandler } from '../lib/common/construct-utils';
 import SingleNewEksCostMonitoringPattern from '../lib/single-new-eks-cost-monitoring-pattern';
 
 const app = configureApp();
 
-new SingleNewEksCostMonitoringPattern(app, 'single-new-eks-cost-monitoring');
+new SingleNewEksCostMonitoringPattern()
+    .buildAsync(app, 'single-new-eks-cost-monitoring')
+    .catch((e) => {
+        errorHandler(app, "Secure Ingress Auth pattern is not setup due to missing secrets for ArgoCD admin pwd. \
+            See Secure Ingress Auth in the readme for instructions", e);
+    });
--- lib/single-new-eks-cost-monitoring-pattern/index.ts
+++ lib/single-new-eks-cost-monitoring-pattern/index.ts
@@ -1,15 +1,37 @@
 import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import { ObservabilityBuilder } from '@aws-quickstart/eks-blueprints';
+import { GlobalResources, LookupHostedZoneProvider, ObservabilityBuilder } from '@aws-quickstart/eks-blueprints';
 import { GrafanaOperatorSecretAddon } from './grafanaoperatorsecretaddon';
 import { KubecostAddOn, KubecostAddOnProps } from '@kubecost/kubecost-eks-blueprints-addon';
 import * as amp from 'aws-cdk-lib/aws-aps';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { setPath } from '@aws-quickstart/eks-blueprints/dist/utils';
+import { prevalidateSecrets } from '../common/construct-utils';
+import CognitoIdpStack from './cognito-idp-stack';
 
-export default class SingleNewEksCostMonitoringPattern {
-    constructor(scope: Construct, id: string) {
+const SECRET_ARGO_ADMIN_PWD = 'argo-admin-secret';
+const gitUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git';
+
+export default class SingleNewEksCostMonitoringPattern extends cdk.Stack {
+    async buildAsync(scope: Construct, id: string) {
+
+        await prevalidateSecrets(SingleNewEksCostMonitoringPattern.name, undefined, SECRET_ARGO_ADMIN_PWD);
+
+        const subdomain: string = blueprints.utils.valueFromContext(scope, "dev.subzone.name", "dev.mycompany.a2z.com");
+        const parentDomain = blueprints.utils.valueFromContext(scope, "parent.hostedzone.name", "mycompany.a2z.com");
+        const certificate: ICertificate = blueprints.getNamedResource(GlobalResources.Certificate);
+
+        const cognitoIdpStackOut = new CognitoIdpStack (scope,'cognito-idp-stack', subdomain,
+            {
+                env: {
+                    account: process.env.CDK_DEFAULT_ACCOUNT,
+                    region: process.env.CDK_DEFAULT_REGION,
+                },
+            });
+
 
         const stackId = `${id}-observability-accelerator`;
         const account = process.env.COA_ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
@@ -93,6 +115,26 @@ export default class SingleNewEksCostMonitoringPattern {
             new blueprints.addons.GrafanaOperatorAddon({
                 createNamespace: true,
             }),
+            new blueprints.ArgoCDAddOn({
+                bootstrapRepo: {
+                    repoUrl: gitUrl,
+                    targetRevision: "main",
+                    path: 'secure-ingress-cognito/envs/dev'
+                },
+                bootstrapValues: {
+                    spec: {
+                        ingress: {
+                            host: subdomain,
+                            cognitoUserPoolArn: cognitoIdpStackOut.userPoolOut.userPoolArn,
+                            cognitoUserPoolAppId: cognitoIdpStackOut.userPoolClientOut.userPoolClientId,
+                            cognitoDomainName: cognitoIdpStackOut.userPoolDomainOut.domainName,
+                            certificateArn: certificate.certificateArn,
+                            region: process.env.CDK_DEFAULT_REGION,
+                        }
+                    },
+                },
+                adminPasswordSecretName: SECRET_ARGO_ADMIN_PWD,
+            }),
             new GrafanaOperatorSecretAddon()
         ];
 
@@ -104,16 +146,18 @@ export default class SingleNewEksCostMonitoringPattern {
             maxSize: 3, 
         };
 
-        ObservabilityBuilder.builder()
+        await ObservabilityBuilder.builder()
             .account(account)
             .region(region)
             .clusterProvider(new blueprints.MngClusterProvider(mngProps))
             .version('auto')
+            .resourceProvider(GlobalResources.HostedZone, new LookupHostedZoneProvider(parentDomain))
+            .resourceProvider(GlobalResources.Certificate, new blueprints.CreateCertificateProvider('secure-ingress-cert', `${subdomain}`, GlobalResources.HostedZone))
             .withAmpProps(ampAddOnProps)
             .resourceProvider(ampWorkspaceName, new blueprints.CreateAmpProvider(ampWorkspaceName, ampWorkspaceName))
             .enableNativePatternAddOns()
             .addOns(...addOns)
-            .build(scope, stackId);
+            .buildAsync(scope, stackId);
     }
 }
 
