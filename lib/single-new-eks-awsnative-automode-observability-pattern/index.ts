d7eac6cc1dd4eeb26ec608356d6d90c9de980a22
Zachary Jacobson <zjacobso@amazon.com>
2025-11-12 13:18:14 -0800

    fix lint

--- lib/single-new-eks-awsnative-automode-observability-pattern/index.ts
+++ lib/single-new-eks-awsnative-automode-observability-pattern/index.ts
@@ -5,29 +5,29 @@ import { ObservabilityBuilder } from '@aws-quickstart/eks-blueprints';
 import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
 
 export default class SingleNewEksAutoModeAWSNativeObservabilityPattern {
-  constructor(scope: Construct, id: string) {
+    constructor(scope: Construct, id: string) {
 
-    const stackId = `${id}-observability-accelerator`;
-    const account = process.env.COA_ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
-    const region = process.env.COA_AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
+        const stackId = `${id}-observability-accelerator`;
+        const account = process.env.COA_ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
+        const region = process.env.COA_AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
 
-    const addOns: Array<blueprints.ClusterAddOn> = [
-      new blueprints.addons.XrayAddOn()
-    ];
+        const addOns: Array<blueprints.ClusterAddOn> = [
+            new blueprints.addons.XrayAddOn()
+        ];
 
-    const cluster = new blueprints.AutomodeClusterProvider({
-      version: KubernetesVersion.V1_33,
-      nodePools: ['system', 'general-purpose']
-    });
+        const cluster = new blueprints.AutomodeClusterProvider({
+            version: KubernetesVersion.V1_33,
+            nodePools: ['system', 'general-purpose']
+        });
 
 
-    ObservabilityBuilder.builder({ isAutoModeCluster: true })
-      .account(account)
-      .region(region)
-      .clusterProvider(cluster)
-      .enableNativePatternAddOns()
-      .enableControlPlaneLogging()
-      .addOns(...addOns)
-      .build(scope, stackId);
-  }
+        ObservabilityBuilder.builder({ isAutoModeCluster: true })
+            .account(account)
+            .region(region)
+            .clusterProvider(cluster)
+            .enableNativePatternAddOns()
+            .enableControlPlaneLogging()
+            .addOns(...addOns)
+            .build(scope, stackId);
+    }
 }
